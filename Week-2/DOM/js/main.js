cae3cbcd8750bf6a521d6c67ecb05ef27f6c06e5
lyaleaz <lyale000az@gmail.com>
2023-11-21 13:46:31 +0200

    a new lesson 4

---
+++ Week-2/DOM/js/main.js
@@ -0,0 +1,1 @@
+const playingField = document.getElementById("playing-field")
---
+++ Week-2/lesson 3/basic-dom-master/main.js
@@ -0,0 +1,10 @@
+console.log(document)
+const playingField = document.getElementById("playing-field")
+console.log(playingField)
+
+const down = document.getElementById("down")
+console.log(down)
+console.log(down.innerHTML)
+down.innerHTML = "Down"
+
+playingField.style.backgroundColor = 'blue'
