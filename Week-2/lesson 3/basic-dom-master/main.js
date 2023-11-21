414f668e7538f59109820e3e83d3db92c0d1662f
lyaleaz <lyale000az@gmail.com>
2023-11-21 22:10:33 +0200

    a new quastion

--- Week-2/lesson 3/basic-dom-master/main.js
+++ Week-2/lesson 3/basic-dom-master/main.js
@@ -1,10 +1,22 @@
-console.log(document)
+/*console.log("document")
 const playingField = document.getElementById("playing-field")
 console.log(playingField)
-
 const down = document.getElementById("down")
 console.log(down)
 console.log(down.innerHTML)
 down.innerHTML = "Down"
 
 playingField.style.backgroundColor = 'blue'
+*/
+const moveRight = function(){
+    const ball = document.getElementById("ball")
+    let left = parseInt(ball.style.left) || 0
+    left += 15
+    ball.style.left = left + "px"
+}
+console.log(moveRight)
+
+const subHeader = document.createElement("h2")
+subHeader.innerHTML = "Game by: ~The Creator~"
+subHeader.setAttribute("class", "sub-header")
+document.body.appendChild(subHeader)
